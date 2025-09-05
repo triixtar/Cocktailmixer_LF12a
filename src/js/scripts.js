6222df2efaca6ebab143e6fb9a00af9842b66d8c
Samuel <samuel.berger@gp-one.com>
2025-09-05 11:01:46 +0200

    added openPopup function

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,3 +4,10 @@ drinkBtn[0].addEventListener("click", function() {
 console.log(this.innerHTML)
 
 })
+function openPopup() {
+    document.getElementById("bgLayer").classList.add('active')
+}
+
+function closePopup() {
+    document.getElementById("bgLayer").classList.remove('active')
+}

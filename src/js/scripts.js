35f64564a531805878d76303018175cde006b6ad
Samuel <samuel.berger@gp-one.com>
2025-09-08 11:00:07 +0200

    popup close on bglayer click

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,16 +1,38 @@
-let drinkBtn = document.getElementsByClassName("drink-button")
-console.log(drinkBtn)
-drinkBtn[0].addEventListener("click", function() {
-console.log(this.innerHTML)
+document.querySelectorAll("button[data-action]").forEach(button => {
+    button.addEventListener("click", () => {
+        const action = button.dataset.action; // "drinkPopup" | "adminPopup"
+        switch (action) {
+            case "drinkPopup":
+                openPopup("drink");
+                break;
+            case "adminPopup":
+                openPopup("pin");
+                break;
+            default:
+                console.warn("Unbekannte Action:", action);
+        }
+    });
+});
 
-})
-function openPopup() {
-    let test = document.getElementById("bgLayer")
-        test.classList.add('active')
-    console.log(test.classList)
+function openPopup(type) {
+    const bgLayer = document.getElementById("bgLayer");
+    const popups = bgLayer.querySelectorAll(".popup");
 
+    popups.forEach(p => p.classList.remove("active"));
+    bgLayer.classList.add("active");
+
+    const popup = bgLayer.querySelector(`.popup-${type}`);
+    if (popup) popup.classList.add("active");
 }
 
 function closePopup() {
-    document.getElementById("bgLayer").classList.remove('active')
+    const bgLayer = document.getElementById("bgLayer");
+    bgLayer.classList.remove("active");
 }
+
+const bgLayer = document.getElementById("bgLayer");
+bgLayer.addEventListener("click", (event) => {
+    if (event.target === bgLayer) {
+        closePopup();
+    }
+});
